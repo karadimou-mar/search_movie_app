840a2caa1927b9dc8e2fce0eb814b59e4b48e4c9
karadimou-mar <karadimou.mar@gmail.com>
2020-02-05 00:05:43 +0200

    Response from API success

--- app/src/main/java/com/example/moviessearchengine/network/MovieAPI.kt
+++ app/src/main/java/com/example/moviessearchengine/network/MovieAPI.kt
@@ -3,14 +3,13 @@ package com.example.moviessearchengine.network
 import com.example.moviessearchengine.model.Movie
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MovieAPI {
 
-    @GET("s={title}")
+    @GET("/")
     fun getMovies(
-        @Path("title") title: String,
-        @Query("apiKey") apiKey: String): Call<Movie>
+        @Query("s") title: String,
+        @Query("apikey") apikey: String): Call<Movie>
 
 }
