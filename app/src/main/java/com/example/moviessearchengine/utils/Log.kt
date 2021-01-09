d05c94d94c100419cd6cb3ac36497e203cff0527
karadimou-mar <karadimou.mar@gmail.com>
2021-01-10 01:03:24 +0200

    utils/log: extensions functions for log implementation were added

--- app/src/main/java/com/example/moviessearchengine/utils/Log.kt
+++ app/src/main/java/com/example/moviessearchengine/utils/Log.kt
@@ -1,2 +1,18 @@
 package com.example.moviessearchengine.utils
 
+import android.util.Log
+
+fun Any.logD(message: String) {
+    Log.d(tag, message)
+}
+
+fun Any.logE(message: String, exception: Throwable? = null) {
+    Log.e(tag, message, exception)
+}
+
+private val Any.tag: String
+    get() {
+        val name = this::class.java.simpleName
+        return if (name.isNotBlank()) name
+        else "Any"
+    }
