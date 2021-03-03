91dae5f7e0c5fe6d5047ead316ff78354b49eef6
Maria Karadimou <maria.karadimou@etraveligroup.com>
2021-03-03 11:37:49 +0200

    app/fonts: fonts added

--- app/src/main/java/com/example/moviessearchengine/utils/View.kt
+++ app/src/main/java/com/example/moviessearchengine/utils/View.kt
@@ -1,2 +1,11 @@
 package com.example.moviessearchengine.utils
 
+import android.view.View
+
+fun View?.visibleElseGone(predicate: View.() -> Boolean) {
+    this?.visibility = if (this?.predicate() == true) View.VISIBLE else View.GONE
+}
+
+fun View?.visibleElseInvisible(predicate: View.() -> Boolean) {
+    this?.visibility = if (this?.predicate() == true) View.VISIBLE else View.INVISIBLE
+}
